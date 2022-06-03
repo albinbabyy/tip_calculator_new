d38d9ca976729cf1a08b6f6a1afc0981196d4618
Albin Baby <albinbaby150@gmail.com>
2022-06-03 10:02:07 +0200

    second condition defined

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ R = first_name.count("R") + second_name.count("R")
 U = first_name.count("U") + second_name.count("U")
 E = first_name.count("E") + second_name.count("E")
 Total = T + R + U + E
+
+L = second_name.count("L") + first_name.count("L")
+O = second_name.count("O") + first_name.count("O")
+V = second_name.count("V") + first_name.count("V")
+E = second_name.count("E") + first_name.count("E")
+Sum = L + O + V + E
