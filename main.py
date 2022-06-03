c6192015afb8a530e4cd08d76142aad419d1ea65
Albin Baby <albinbaby150@gmail.com>
2022-06-03 10:01:17 +0200

    first condition defined

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
+
+first_name = name1.upper()
+second_name = name2.upper()
+T = first_name.count("T") + second_name.count("T")
+R = first_name.count("R") + second_name.count("R")
+U = first_name.count("U") + second_name.count("U")
+E = first_name.count("E") + second_name.count("E")
+Total = T + R + U + E
